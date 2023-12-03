d7837ccd0a45c7161aa52594643d55e43ca76a91
afdinal <afdinalbtb16@gmail.com>
2023-12-03 18:33:52 +0700

    inititating table func

--- tools/table.py
+++ tools/table.py
@@ -29,33 +29,37 @@ properties_data = {
 }
 
 ############ Properties Calculation ############
-# from Properties import Oil_Prop;
-# def Bo_Data():
-#     Oil_Prop.Bo_Standing()
+from Properties import Oil_Prop;
+from tools import calculator;
 
-# df = pd.DataFrame(properties_data);
+data = calculator.userInputData;
+
+
+
+############ Tabel Development ############
+df = pd.DataFrame(properties_data);
         
-# def openExcelApp():
-#     showTableTittle();
-#     excel_file_path = 'C:\\PVT_Table.xlsx';
-#     with pd.ExcelWriter(excel_file_path) as writer:
-#         df.to_excel(writer, sheet_name='PVT Table');
+def openExcelApp():
+    showTableTittle();
+    excel_file_path = 'C:\\PVT_Table.xlsx';
+    with pd.ExcelWriter(excel_file_path) as writer:
+        df.to_excel(writer, sheet_name='PVT Table');
         
-#     userInput = input(colorama.Fore.CYAN + "Open Excel? [yes/no] ").lower();
-#     if userInput == 'yes':
-#         if os.path.exists(excel_file_path):
-#             workbook = load_workbook(excel_file_path);
-#             workbook.save(excel_file_path);
+    userInput = input(colorama.Fore.CYAN + "Open Excel? [yes/no] ").lower();
+    if userInput == 'yes':
+        if os.path.exists(excel_file_path):
+            workbook = load_workbook(excel_file_path);
+            workbook.save(excel_file_path);
         
-#             try:
-#                 os.system('start excel.exe "%s"' %excel_file_path);
-#                 print(colorama.Fore.BLUE + "Successfully opened the file!\n");
-#             except:
-#                 print(colorama.Fore.RED + "Something went wrong!\n");
-#         else:
-#             print(colorama.Fore.RED + "File not found!\n");
-#     elif userInput == 'no':
-#         print(colorama.Fore.LIGHTCYAN_EX + f"Good bye then!\nMake sure you check the .xlsx file in {excel_file_path}\n");
-#     else:
-#         print(colorama.Fore.RED + "Command not detected as a valid input! This session will be terminated to avoid errors\n");
+            try:
+                os.system('start excel.exe "%s"' %excel_file_path);
+                print(colorama.Fore.BLUE + "Successfully opened the file!\n");
+            except:
+                print(colorama.Fore.RED + "Something went wrong!\n");
+        else:
+            print(colorama.Fore.RED + "File not found!\n");
+    elif userInput == 'no':
+        print(colorama.Fore.LIGHTCYAN_EX + f"Good bye then!\nMake sure you check the .xlsx file in {excel_file_path}\n");
+    else:
+        print(colorama.Fore.RED + "Command not detected as a valid input! This session will be terminated to avoid errors\n");
 
